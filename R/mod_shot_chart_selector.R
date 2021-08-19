260a3c40ef95b8770b6fd55b99649f299eeec051
Henri Freixe <henri.freixe@edhec.com>
2021-08-19 19:28:02 +0200

    No selected Player

--- R/mod_shot_chart_selector.R
+++ R/mod_shot_chart_selector.R
@@ -18,8 +18,7 @@ mod_shot_chart_selector_ui <- function(id){
     column(6,
            selectInput(ns("player"),
                        label = "Select a Player",
-                       choices = scope_players("2020-21"),
-                       selected = "Stephen Curry"))
+                       choices = scope_players("2020-21")))
     ),
     actionButton(ns("change"),"Visualise", class = "btn-success"),
     downloadButton(ns("download"),"Download Visualisation",class = "btn-success"),
